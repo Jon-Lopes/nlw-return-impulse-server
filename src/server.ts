1b3fc0409683db3fb91136a5db10d67068b3d3e0
Jonatas S Lopes <jonatas.siq.lopes@gmail.com>
2022-05-09 11:02:20 -0400

    Add env port variable

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(routes);
     // return res.send('Hello World!');
 // })
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server running!');
-})
+});
 
